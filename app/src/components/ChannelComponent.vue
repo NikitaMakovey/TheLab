a88869c038519877c4814f3e46dd9e9bcc4b4b55
lppttssa <lppttssa@gmail.com>
2020-04-24 20:51:43 +1000

    Add context menu

--- app/src/components/ChannelComponent.vue
+++ app/src/components/ChannelComponent.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>
+    <div id="app" @contextmenu.prevent="$refs.menu.open">
+        <vue-context ref="menu">
+            <li>
+                    <v-dialog v-model="aboutDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+                        <template v-slot:activator="{ on }">
+                            <span v-on="on">Ouscilograma</span>
+                        </template>
+                        <v-card>
+                            <v-toolbar dark color="black">
+                                <v-toolbar-title>Ouscilogramma</v-toolbar-title>
+                                <v-spacer></v-spacer>
+                            </v-toolbar>
+                            <v-list three-line subheader>
+                                <v-subheader class="title">Ouscilogramma of canals</v-subheader>
+                                <div>
+                                    <v-list-item-content>
+                                        <v-list-item-title class="title text--accent-3">Here you can see your canals</v-list-item-title>
+                                        <v-list-item-subtitle></v-list-item-subtitle>
+                                    </v-list-item-content>
+                                </div>
+                            </v-list>
+                        </v-card>
+                    </v-dialog>
+            </li>
+        </vue-context>
         <canvas height="100" width="200" style="border: 1px solid black; padding: 1px;" ref="canvas"></canvas>
     </div>
 </template>
 
 <script>
     import { Line, mixins } from 'vue-chartjs'
+    import Vue from 'vue'
+    import { VueContext } from 'vue-context'
+     import 'vue-context/src/sass/vue-context.scss';
+
+    
 
     export default {
+        components: {
+            VueContext
+        },
         extends: Line,
         props: ['chartData', 'chartName'],
         mixins: [mixins.reactiveProp],
@@ -25,7 +57,7 @@
                             borderColor: 'black'
                         }
                     ]
-                },
+            },
                 {
                     scales:{
                         xAxes: [{
@@ -37,8 +69,15 @@
                     },
                     responsive: true
                 });
-        }
+        },
+        methods: {
+            onClick (text) {
+                alert(`You clicked ${text}!`);
+            }
+        },
+        
     }
+
 </script>
 
 <style scoped>
